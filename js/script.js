e420ce243b58a504cf8acd146c6b3da0b1c9021a
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-18 16:44:54 +0300

    FOR

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 //Первый цикл будет складывать все елементы массива.
-//var num = [1, 3, 2, 5, 6];
-//summa = 0;
-//for (var i = 0; i < num.length; i++) {
-    //summa += num[i];
-//}
-//console.log(summa);
+var num = [1, 3, 2, 5, 6];
+summa = 0;
+for (var i = 0; i < num.length; i++) {
+    summa += num[i];
+}
+console.log(summa);
 
 //Цикл, которыйй считает сумму квадратов элементов массива.
-//var num = [1, 3, 2, 5, 6];
-//summa = 0;
-//for (var i = 0; i < num.length; i++) {
-    //summa += num[i] * num[i];
-//}
-//console.log(summa);
+var num = [1, 3, 2, 5, 6];
+summa = 0;
+for (var i = 0; i < num.length; i++) {
+    summa += num[i] * num[i];
+}
+console.log(summa);
 
+//Цикл, который считает среднее орифметическое массива.
 var num = [1, 3, 2, 5, 6];
 summa = 0;
 for (var i = 0; i < num.length; i++) {
