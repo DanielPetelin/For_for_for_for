527d3938c2ec7131c57ed7fdf3aa8c367ea1822f
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-18 16:34:18 +0300

    /

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
+//Первый цикл будет складывать все елементы массива.
 var num = [1, 3, 2, 5, 6];
 summa = 0;
 for (var i = 0; i < num.length; i++) {
     summa += num[i];
 }
-console.log(summa);
+//console.log(summa);
